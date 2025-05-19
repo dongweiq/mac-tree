a612ae82a1a84b9f33f3b67cdefe1144a76a14b7
王洪贺 <dongweiqmail@gmail.com>
2025-05-19 14:53:24 +0800

    added: 树形结构分析

--- src/accessibility.mm
+++ src/accessibility.mm
@@ -59,9 +59,46 @@ Napi::Value GetSystemWideElement(const Napi::CallbackInfo& info) {
     });
 }
 
+Napi::Value GetAppMainWindowElement(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    
+    if (!info[0].IsNumber()) {
+        Napi::TypeError::New(env, "Wrong arguments, expected PID number").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    pid_t pid = info[0].As<Napi::Number>().Int32Value();
+    AXUIElementRef appRef = AXUIElementCreateApplication(pid);
+    
+    if (!appRef) {
+        return env.Null();
+    }
+
+    // 获取应用的窗口
+    CFArrayRef windowArray;
+    AXError result = AXUIElementCopyAttributeValue(appRef, kAXWindowsAttribute, (CFTypeRef*)&windowArray);
+    
+    if (result != kAXErrorSuccess || !windowArray) {
+        CFRelease(appRef);
+        return env.Null();
+    }
+
+    // 获取第一个窗口（主窗口）
+    AXUIElementRef windowRef = (AXUIElementRef)CFArrayGetValueAtIndex(windowArray, 0);
+    CFRetain(windowRef);  // 增加引用计数
+    
+    CFRelease(windowArray);
+    CFRelease(appRef);
+
+    return Napi::External<void>::New(env, (void*)windowRef, [](Napi::Env env, void* data) {
+        CFRelease((AXUIElementRef)data);
+    });
+}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
     exports.Set("getSystemWideElement", Napi::Function::New(env, GetSystemWideElement));
     exports.Set("getElementAttributes", Napi::Function::New(env, GetElementAttributes));
+    exports.Set("getAppMainWindowElement", Napi::Function::New(env, GetAppMainWindowElement));
     return exports;
 }
 
